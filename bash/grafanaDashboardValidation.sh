a2c205a41cd9501838f9e62c3e0537ec2dad8761
No Sleep <49619557+nosleep77@users.noreply.github.com>
2022-12-04 13:07:02 -0500

    Create grafanaDashboardValidation.sh

---
+++ bash/grafanaDashboardValidation.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set -e
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+NC='\033[0m'    
+
+Env=$1
+Version=$2
+Grafana_URL=$3
+Token=$4           
+#
+# ./script.sh dev 0 glsa_2NuXsP9GIVbIiztGgvju9EnJLd6gctTg_6f2708af localhost
+#curl -s http://admin:yGrXQepEA-s-hg==@127.0.0.1:3000/api/searchh?dashboard_uid=d111e6dc08db1d3cd5e306b601de265f6a3944ab | jq -r ' .[] | select(.type == "dash-db") | .uid'
+#curl -s http://admin:yGrXQepEA-s-hg==@localhost:3000/api/dashboards/uid/c6090637d2f5c6b2c1527a0e3f66eaa29522ac44 | jq '.'
+#curl -s --location --request GET 'http://localhost:3000/api/search' --header "Authorization: Bearer $4" | jq -r ' .[] | select(.type == "dash-db") | .uid'
+
+
+printf "\n"
+
+#Grabing all yaml files and store their relative path in array
+mapfile -d $'\0' array < <(find ./deployment/$Env/ -type f -iname "*.yml" -or -iname "*.yaml" -print0)
+
+#Filtering Yaml files. Keeping only Grafandashboard CRs
+new_array=()
+for i in "${array[@]}"
+do
+   : 
+    x=$(myenv="GrafanaDashboard" yq e ' select(.kind == env(myenv)) | .kind' $i)
+    if [ "$x" = "GrafanaDashboard" ]; then
+       new_array+=($i)
+    fi
+done
+
+len1=${#new_array[*]};
+echo "Found ${len1} Yaml files of kind GrafanaDashboard in the deployment folder ./deployment/$Env/ :"
+#Printing the name of the Grafanadashboard CRs 
+for i in "${new_array[@]}"
+do
+   : 
+    printf "   ${GREEN} $i ${NC}\n"
+done
+printf "\n"
+
+#Save the name of dashboards and version in an array
+k8s_crs_names=$(kubectl get grafanadashboards -n monitoring -o jsonpath="{.items[*].metadata.name}")
+k8s_crs_names_array=($(echo "$k8s_crs_names" | tr ' ' '\n'))
+
+k8s_uids_array=()
+k8s_ver_array=()
+#Store uid and version from k8s in seperate arrays
+for i in "${k8s_crs_names_array[@]}"
+do
+    :
+    k8s_uids_array+=($(kubectl get grafanadashboard  $i -n monitoring  -o json | jq -r '.spec.json' | jq -r '.uid'))
+    k8s_ver_array+=($(kubectl get grafanadashboard  $i -n monitoring  -o json | jq -r '.metadata.labels.version'))
+done
+
+#Checking k8s
+printf "${PURPLE}\t\t******** kubernetes CR check ********\n${NC}"
+
+for i in "${new_array[@]}"
+do
+   : 
+    #grabbing the uid from the yaml file
+    cr_uid=$(yq -o=json eval $i | jq '.spec' | jq -r '.json' |  jq -r '.uid')
+    match_bool=0
+    for j in k8s_uid"${!k8s_uids_array[@]}"
+    do
+        if [ "${k8s_uids_array[$j]}" == "$cr_uid" ] ; then  
+        # If the above condition is evaluated to tue that means we found an uid match
+        # The followong if is to check if there is a version match as well
+            if [ "${k8s_ver_array[$j]}" == "$Version" ] ; then 
+                printf "${BLUE}\ndashboard with UID: $cr_uid and version: $Version in kubernetes - found\n${NC}"
+                match_bool=1   
+            fi
+        fi
+    done
+    if [ $match_bool -eq "0" ] ; then 
+            printf "${RED}\ndashboard with UID: $cr_uid and version: $Version in kubernetes - Not found\n${NC}"
+    fi
+done
+#check grafana
+
+printf "${PURPLE}\n\t\t******** Grafana API check ********\n${NC}"
+
+grafana_uids=$(curl -s --location --request GET "http://$Grafana_URL/api/search" --header "Authorization: Bearer $Token" | jq -r ' .[] | select(.type == "dash-db") | .uid')
+grafana_uid_array=($(echo "$grafana_uids" | tr ' ' '\n'))
+for i in "${new_array[@]}"
+do
+   : 
+    cr_uid=$(yq -o=json eval $i | jq '.spec' | jq -r '.json' |  jq -r '.uid')
+    match_bool=0
+    for j in "${grafana_uid_array[@]}" 
+    do
+        :
+        if [ "$j" == "$cr_uid" ] ; then
+                printf "${BLUE}\n Dashboard with UID: $cr_uid and version: $Version in Grafana - found\n${NC}"
+                match_bool=1
+        fi
+    done
+    if [ $match_bool -eq "0" ] ; then 
+            printf "${RED}\n Dashboard with UID: $cr_uid and version: $Version in Grafana- not found ${NC}\n"
+   fi
+
+done
+printf "\n"
