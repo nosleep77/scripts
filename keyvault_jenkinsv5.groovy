fd04d173d0851ba204febf4c67e7e67c31bdc4e6
No Sleep <49619557+nosleep77@users.noreply.github.com>
2022-12-09 18:30:38 -0500

    Create keyvault_jenkinsv5.groovy

---
+++ keyvault_jenkinsv5.groovy
@@ -0,0 +1,33 @@
+pipeline {
+  stages {
+    stage('Run Script') {
+      steps {
+          script {
+            env.secret=FetchSecret("api","jenkins-keyvault-azure")
+            sh 'curl -s -o script.sh https://raw.githubusercontent.com/Ryder05/AwsomeProject/main/script.sh && chmod u+x script.sh '
+            sh """ ./script.sh ${env.secret} """ }
+            }
+        }
+    }
+}
+
+
+String FetchSecret(String secretName,String vaultName) {
+    withCredentials([azureServicePrincipal(credentialsId: 'azure-sp',
+                      subscriptionIdVariable: 'SUBS_ID',
+                      clientIdVariable: 'CLIENT_ID',
+                      clientSecretVariable: 'CLIENT_SECRET',
+                      tenantIdVariable: 'TENANT_ID')]) {
+        sh  """
+            set +x
+            az version 
+            az account clear
+            az login --service-principal -u $CLIENT_ID  -p $CLIENT_SECRET  -t $TENANT_ID
+            echo 'set current subcription to source subscription'
+            az account set --subscription $SUBS_ID
+            """ 
+        def secret = sh script:"az keyvault secret show --name ${secretName} --vault-name ${vaultName} --query value -o tsv", returnStdout: true
+        return secret //exporting to secret as env var
+}        
+}
+
