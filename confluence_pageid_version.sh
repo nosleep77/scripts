3fe962fab255382e6150482aad8b565799ae3798
No Sleep <49619557+nosleep77@users.noreply.github.com>
2023-01-05 11:22:40 -0500

    Update confluence_pageid_version.sh

--- confluence_pageid_version.sh
+++ confluence_pageid_version.sh
@@ -18,3 +18,38 @@ echo "Page version: $page_version"
 
 
 curl -u username:password -X POST -H "Content-Type: text/html" --data-binary @file.html "https://your-confluence-site.com/rest/api/content/"
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+# Set the login credentials and page ID
+USERNAME='username'
+PASSWORD='password'
+PAGE_ID='page-id'
+
+# Set the URL of the Confluence site
+CONFLUENCE_URL='https://your-confluence-site.com/'
+
+# Read the HTML file into a variable
+HTML=$(<file.html)
+
+# Encode the HTML content as a URL parameter
+HTML_ENCODED=$(printf "%s" "$HTML" | jq -R -s -r @uri)
+
+# Set the JSON payload for the update request
+JSON='{"id":"'$PAGE_ID'","type":"page","title":"Page Title","space":{"key":"SPACEKEY"},"body":{"storage":{"value":"'$HTML_ENCODED'","representation":"storage"}}}'
+
+# Send the update request to the Confluence REST API
+curl -u "$USERNAME:$PASSWORD" -X PUT -H "Content-Type: application/json" -d "$JSON" "$CONFLUENCE_URL/rest/api/content/$PAGE_ID"
+
+
